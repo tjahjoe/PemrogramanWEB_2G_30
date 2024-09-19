298e69fc78c59f66cfca4b6743245db72f6d9aba
tjahjoe <ntell634@gmail.com>
2024-09-19 18:49:55 +0700

    P4_Praktikum5_Soal5.2_30

--- dasarWeb/Pertemuan4/array.php
+++ dasarWeb/Pertemuan4/array.php
@@ -1,3 +1,4 @@
+<!-- soal 5.1 -->
 <?php
 $nilaiSiswa = [85, 92, 78, 64, 90, 55, 88, 79, 70, 96];
 
@@ -9,4 +10,27 @@ foreach ($nilaiSiswa as $nilai) {
 }
 
 echo "Daftar nilai siswa yang lulus: " . implode(', ', $nilaiLulus);
+?>
+
+<!-- soal 5.2 -->
+<br>
+<hr>
+<?php
+$daftarKaryawan = [
+    ["Alice", 7],
+    ["Bob", 3],
+    ["Charlie", 9],
+    ["David", 5],
+    ["Eva", 6]
+];
+
+$karyawanPengalamLimaTahun = [];
+
+foreach($daftarKaryawan as $karyawan){
+    if($karyawan[1] > 5){
+        $karyawanPengalamLimaTahun[] = $karyawan[0];
+    }
+}
+
+echo "Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun: " . implode(', ', $karyawanPengalamLimaTahun)
 ?>
