e9698fe59197235cede097c516da839fd9217d43
tjahjoe <ntell634@gmail.com>
2024-09-19 18:22:28 +0700

    P4_Praktikum4_Soal4.8_30

---
+++ dasarWeb/Pertemuan4/soal_praktikum4_8.php
@@ -0,0 +1,11 @@
+<p>
+<?php
+$poin = 555;
+$hadiahTambahan = $poin > 500 ? "YA" : "TIDAK";
+echo 
+"
+Total skor pemain adalah: $poin <br>
+Apakah pemain mendapatkan hadiah tambahan? $hadiahTambahan
+";
+?>
+</p>
