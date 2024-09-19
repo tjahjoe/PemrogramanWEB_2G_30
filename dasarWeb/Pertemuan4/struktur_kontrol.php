2ad707de9e9a430f3aa86ff50ddc1fe668aafe51
tjahjoe <ntell634@gmail.com>
2024-09-19 13:02:10 +0700

    P4_Praktikum4_Soal_4.2_30

--- dasarWeb/Pertemuan4/struktur_kontrol.php
+++ dasarWeb/Pertemuan4/struktur_kontrol.php
@@ -1,5 +1,31 @@
 <?php
 $nilaiNumerik = 92;
+?>
+<table border=1 style="text-align : center;">
+    <tr>
+        <th>Nilai Angka</th>
+        <th>Nilai huruf</th>
+    </tr>
+    <tr>
+        <td>90 <= nilai <= 100</td>
+        <td>A</td>
+    </tr>
+    <tr>
+        <td>80 <= nilai < 90</td>
+        <td>B</td>
+    </tr>
+    <tr>
+        <td>70 <= nilai < 80</td>
+        <td>C</td>
+    </tr>
+    <tr>
+        <td>nilai < 70</td>
+        <td>D</td>
+    </tr>
+</table>
+<br>
+<?php
+echo "Nilai angka: $nilaiNumerik<br>";
 if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
     echo "Nilai huruf: A";
 } else if ($nilaiNumerik >= 80 && $nilaiNumerik < 90) {
@@ -9,4 +35,24 @@ if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
 } else if ($nilaiNumerik < 70) {
     echo "Nilai huruf: D";
 }
+echo "<br><br><hr>";
+
+$jarakSaatIni = 0;
+$jarakTarget = 500;
+$peningkatanHarian = 30;
+$hari = 0;
+
+echo 
+"
+jarak awal = $jarakSaatIni <br>
+target jarak = $jarakTarget <br>
+penambahan jarak dalam satu hari = $peningkatanHarian <br>
+";
+
+while ($jarakSaatIni < $jarakTarget) {
+    $jarakSaatIni += $peningkatanHarian;
+    $hari++;
+}
+
+echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
 ?>
