b039c234aa376f944a26b1b7a59d181f6a1e1a37
tjahjoe <ntell634@gmail.com>
2024-09-27 16:21:40 +0700

    P5_Praktikum1_Soal4_30

--- dasarWeb/Pertemuan5/php 2/fungsi.php
+++ dasarWeb/Pertemuan5/php 2/fungsi.php
@@ -12,3 +12,14 @@ $ucapanSalam = "Selamat pagi";
 
 perkenalan($saya);
 ?>
+
+<br><br>
+
+<?php
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+
+echo "Umur saya adalah " . hitungUmur(2004, 2024) . "tahun";
+?>
