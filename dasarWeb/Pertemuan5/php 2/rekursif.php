782df09a071e7261a8e2da54b90512b1bfb38db2
tjahjoe <ntell634@gmail.com>
2024-09-27 17:34:30 +0700

    P5_Praktikum3_Soal7_30

--- dasarWeb/Pertemuan5/php 2/rekursif.php
+++ dasarWeb/Pertemuan5/php 2/rekursif.php
@@ -9,18 +9,63 @@
 ?>
 
 <?php
-for ($i=1; $i <= 25; $i++) { 
-    echo "Perulangan ke-{$i} <br>";
-}
+// for ($i=1; $i <= 25; $i++) { 
+//     echo "Perulangan ke-{$i} <br>";
+// }
 ?>
 
 <?php
-function tampilkanAngka(int $jumlah, int $indeks = 1){
-    echo "Perulangan ke-{$indeks}<br>";
+// function tampilkanAngka(int $jumlah, int $indeks = 1){
+//     echo "Perulangan ke-{$indeks}<br>";
 
-    if ($indeks < $jumlah) {
-        tampilkanAngka($jumlah, $indeks + 1);
+//     if ($indeks < $jumlah) {
+//         tampilkanAngka($jumlah, $indeks + 1);
+//     }
+// }
+// tampilkanAngka(20);
+?>
+
+<?php
+$menu = [
+    [
+        "nama" => "Beranda",
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai",
+                    ],
+                    [
+                        "nama" => "Gunung",
+                    ],
+                ],
+            ],
+            [
+                "nama" => "Kuliner",
+            ],
+            [
+                "nama" => "Hiburan",
+            ],
+        ],
+    ],
+    [
+        "nama" => "Tentang",
+    ],
+    [
+        "nama" => "Kontak",
+    ],
+];
+
+function tampikanMenuBertingkat(array $menu){
+    echo "<ul>";
+    foreach($menu as $key => $item){
+        echo "<li>{$item['nama']}</li>";
     }
-}
-tampilkanAngka(20);
+    echo "</ul>";
+};
+tampikanMenuBertingkat($menu);
 ?>
