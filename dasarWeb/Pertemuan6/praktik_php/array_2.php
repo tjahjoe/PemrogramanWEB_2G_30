864da0b4b1bb1866711b983b1d00403361a0e398
tjahjoe <ntell634@gmail.com>
2024-10-03 18:47:14 +0700

    P6_Praktikum2_30

---
+++ dasarWeb/Pertemuan6/praktik_php/array_2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <?php
+    $Dosen = [
+        'nama' => 'Elok Nur Hamdana',
+        'domisili' => 'Malang',
+        'jenis_kelamin' => 'Perempuan'
+    ];
+    echo "Nama : {$Dosen['nama']} <br>";
+    echo "Domisili : {$Dosen['domisili']} <br>";
+    echo "Jenis Kelamin : {$Dosen['jenis_kelamin']} <br>";
+    ?>
+</body>
+</html>
