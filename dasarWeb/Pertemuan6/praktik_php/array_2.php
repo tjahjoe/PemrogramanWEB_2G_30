66a84b73b01ecab14a8466b94cd41b7ac8deeb03
tjahjoe <ntell634@gmail.com>
2024-10-03 19:58:01 +0700

    P6_Praktikum2_Soal2_30

--- dasarWeb/Pertemuan6/praktik_php/array_2.php
+++ dasarWeb/Pertemuan6/praktik_php/array_2.php
@@ -3,18 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="array_2.css">
     <title></title>
 </head>
 <body>
-    <?php
-    $Dosen = [
+    <div class="box">
+        <img src="image.png" class="image">   
+        <table>
+        <?php
+        $Dosen = [
         'nama' => 'Elok Nur Hamdana',
         'domisili' => 'Malang',
         'jenis_kelamin' => 'Perempuan'
     ];
-    echo "Nama : {$Dosen['nama']} <br>";
-    echo "Domisili : {$Dosen['domisili']} <br>";
-    echo "Jenis Kelamin : {$Dosen['jenis_kelamin']} <br>";
+    foreach($Dosen as $x => $y){
+        $temporary = ucfirst(preg_replace('/[^A-Za-z0-9\-]/', ' ', $x)); 
+        echo "<tr><td>$temporary</td><td>: $y</td></tr>";
+    }
     ?>
+    </table>
+    </div>
 </body>
 </html>
