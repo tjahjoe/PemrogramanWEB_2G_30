9441d21125d22995cc150029a0df4b36d261801c
tjahjoe <ntell634@gmail.com>
2024-10-03 18:36:20 +0700

    P6_Praktikum1_30

---
+++ dasarWeb/Pertemuan6/praktik_php/array_1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h2>Array Terindeks</h2>
+        <?php
+        $ListDosen = ["Elok Nur Hamdana", "Unggul Pemenang", "Bagas Nugraha"];
+        echo $ListDosen[2] . "<br>";
+        echo $ListDosen[0] . "<br>";
+        echo $ListDosen[1] . "<br>";
+        ?>
+    </body>
+</html>
