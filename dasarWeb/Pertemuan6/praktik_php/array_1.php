2cad0ede98b40a26d9683ce9bfc6036677847847
tjahjoe <ntell634@gmail.com>
2024-10-03 18:37:08 +0700

    P6_Praktikum1_Soal1_30

--- dasarWeb/Pertemuan6/praktik_php/array_1.php
+++ dasarWeb/Pertemuan6/praktik_php/array_1.php
@@ -6,10 +6,14 @@
     <body>
         <h2>Array Terindeks</h2>
         <?php
-        $ListDosen = ["Elok Nur Hamdana", "Unggul Pemenang", "Bagas Nugraha"];
-        echo $ListDosen[2] . "<br>";
-        echo $ListDosen[0] . "<br>";
-        echo $ListDosen[1] . "<br>";
+        $ListDosen = [
+            "Elok Nur Hamdana", 
+            "Unggul Pemenang", 
+            "Bagas Nugraha"
+        ];
+        for ($i=0; $i < count($ListDosen); $i++) { 
+            echo $ListDosen[$i] . "<br>";
+        }
         ?>
     </body>
 </html>
