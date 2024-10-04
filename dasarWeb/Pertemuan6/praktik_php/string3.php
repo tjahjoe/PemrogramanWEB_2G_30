acf9841e08018ff66e8991a017a74a8f3f7c3b65
tjahjoe <ntell634@gmail.com>
2024-10-04 08:08:18 +0700

    P6_Praktikum6_Soal16_30

--- dasarWeb/Pertemuan6/praktik_php/string3.php
+++ dasarWeb/Pertemuan6/praktik_php/string3.php
@@ -1,4 +1,8 @@
 <?php
 $pesan = "Saya arek malang";
-echo strrev($pesan) . "<br>"
+// echo strrev($pesan) . "<br>"
+$pesanPerKata = explode(" ", $pesan);
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+$pesan = implode(" ", $pesanPerKata);
+echo $pesan . "<br>"
 ?>
