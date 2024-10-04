9e7e8098f11d68329b03f7e091fb9e49ec57a64b
tjahjoe <ntell634@gmail.com>
2024-10-04 07:37:48 +0700

    P6_Praktikum4_Soal5_30

--- dasarWeb/Pertemuan6/praktik_php/fungsi.php
+++ dasarWeb/Pertemuan6/praktik_php/fungsi.php
@@ -1,9 +1,38 @@
 <?php
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Wahyu<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
+// function perkenalan($nama, $salam="Assalamualaikum"){
+//     echo $salam. ", ";
+//     echo "Perkenalkan, nama saya " . $nama ."<br/>";
+//     echo "Senang berkenalan dengan Anda<br/>";
+// }
+// perkenalan("Wahyu", "Hallo");
+// echo "<hr>";
+
+// $saya = "Wahyu";
+// $ucapanSalam = "Selamat pagi";
+
+// perkenalan($saya);
+?>
+
+<!-- <br><br> -->
+<?php
+// function hitungUmur($thn_lahir, $thn_sekarang){
+//     $umur = $thn_sekarang - $thn_lahir;
+//     return $umur;
+// }
+// echo "Umur saya adalah " . hitungUmur(2004, 2024) . "tahun";
+?>
+
+<?php
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
 }
-perkenalan();
-perkenalan();
+
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo $salam . ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br/>";
+    echo "Saya berusia " . hitungUmur(2004, 2024) . " tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+perkenalan("Wahyu")
 ?>
