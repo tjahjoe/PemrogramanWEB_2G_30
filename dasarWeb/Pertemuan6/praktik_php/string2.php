0fb3919de9206d3a7ab569f380634ee741a06e7b
tjahjoe <ntell634@gmail.com>
2024-10-04 08:02:55 +0700

    P6_Praktikum6_Soal14_30

---
+++ dasarWeb/Pertemuan6/praktik_php/string2.php
@@ -0,0 +1,10 @@
+<?php
+echo "Baris\nbaru <br>";
+echo 'Baris\nbaru <br>';
+echo "Halo\rDunia <br>";
+echo 'Helo\rDunia <br>';
+echo "<pre>Halo\tDunia!</pre>";
+echo '<pre>Halo\tDunia!</pre>';
+echo "Katakanlah \"Tidak pada narkoba!\" <br>";
+echo 'Katakanlah \'Tidak pada narkoba!\' <br>';
+?>
