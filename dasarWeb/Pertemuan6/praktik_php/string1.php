99805cf27d2ced0e6d125f79a38d75cbc7fc6b67
tjahjoe <ntell634@gmail.com>
2024-10-04 07:59:57 +0700

    P6_Praktikum6_Soal13_30

---
+++ dasarWeb/Pertemuan6/praktik_php/string1.php
@@ -0,0 +1,12 @@
+<?php
+$loremIpsum = "Lorem ipsum dolor sit amet consectetur 
+adipisicing elit. Laborum, blanditiis quam dolorem 
+ratione iste consequuntur et fugit exercitationem. Porro 
+voluptates earum itaque eum amet delectus incidunt culpa 
+dolores excepturi laboriosam.";
+echo "<p>{$loremIpsum}</p>";
+echo "Panjang Karakter: " . strlen($loremIpsum) .  "<br>";
+echo "Panjang kata: " . str_word_count($loremIpsum) . "<br>";
+echo "<p>" . strtoupper($loremIpsum) . "</p>";
+echo "<p>" . strtolower($loremIpsum) . "</p>";
+?>
