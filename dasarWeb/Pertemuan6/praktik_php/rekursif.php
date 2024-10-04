981ebd1669dcc8d706dc4e7ea54166cdd25599c9
tjahjoe <ntell634@gmail.com>
2024-10-04 07:50:13 +0700

    P6_Praktikum5_Soal10_30

--- dasarWeb/Pertemuan6/praktik_php/rekursif.php
+++ dasarWeb/Pertemuan6/praktik_php/rekursif.php
@@ -1,7 +1,23 @@
 <?php
-function tampilkanHaloDunia(){
-    echo "Halo dunia! <br>";
-    tampilkanHaloDunia();
+// function tampilkanHaloDunia(){
+//     echo "Halo dunia! <br>";
+
+//     tampilkanHaloDunia();
+// }
+// tampilkanHaloDunia();
+?>
+<?php
+for ($i=1; $i <= 25; $i++) { 
+    echo "Perulangan ke-{$i} <br>";
 }
-tampilkanHaloDunia();
+?>
+
+<?php
+function tampilkanAngka(int $jumlah, int $indeks = 1){
+    echo "Perulangan ke-{$indeks}<br>";
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+tampilkanAngka(20);
 ?>
