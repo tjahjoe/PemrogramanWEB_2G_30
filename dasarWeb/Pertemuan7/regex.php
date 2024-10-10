1c0007d338a078538bf415593951fa4b7e5cd88e
tjahjoe <ntell634@gmail.com>
2024-10-10 17:35:49 +0700

    P7_Praktikum5_Soal5.2_30

--- dasarWeb/Pertemuan7/regex.php
+++ dasarWeb/Pertemuan7/regex.php
@@ -1,9 +1,19 @@
 <?php
-$pattern ='/[a-z]/';
-$text = 'This is a Sample Text.';
-if (preg_match($pattern, $text)) {
-    echo 'Huruf kecil ditemukan!';
+// $pattern ='/[a-z]/';
+// $text = 'This is a Sample Text.';
+// if (preg_match($pattern, $text)) {
+//     echo 'Huruf kecil ditemukan!';
+// } else {
+//     echo 'Tidak ada huruf kecil!';
+// }
+?>
+
+<?php
+$pattern = '/[0-9]+/';
+$text = 'There are 123 apples.';
+if(preg_match($pattern, $text, $matches)){
+    echo 'Cocokkan: ' .$matches[0];
 } else {
-    echo 'Tidak ada huruf kecil!';
+    echo 'Tidak ada yang cocok!';   
 }
 ?>
