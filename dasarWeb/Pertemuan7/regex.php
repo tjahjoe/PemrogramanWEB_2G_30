52717d6485cbacc851cb34b062bc2efd5408336e
tjahjoe <ntell634@gmail.com>
2024-10-10 17:54:40 +0700

    P7_Praktikum5_Soal5.3_30

--- dasarWeb/Pertemuan7/regex.php
+++ dasarWeb/Pertemuan7/regex.php
@@ -9,11 +9,20 @@
 ?>
 
 <?php
-$pattern = '/[0-9]+/';
-$text = 'There are 123 apples.';
-if(preg_match($pattern, $text, $matches)){
-    echo 'Cocokkan: ' .$matches[0];
-} else {
-    echo 'Tidak ada yang cocok!';   
-}
+// $pattern = '/[0-9]+/';
+// $text = 'There are 123 apples.';
+// if(preg_match($pattern, $text, $matches)){
+//     echo 'Cocokkan: ' .$matches[0];
+// } else {
+//     echo 'Tidak ada yang cocok!';   
+// }
 ?>
+
+<?php
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = 'I like apple pie.';
+$new_text = preg_replace($pattern, $replacement, $text);
+echo $new_text;
+?>
+
