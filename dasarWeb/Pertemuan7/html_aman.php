63b909d436fba590f7ee554f2e709760bc2ce84e
tjahjoe <ntell634@gmail.com>
2024-10-10 13:00:59 +0700

    P7_Praktikum4_Soal4.1_30

---
+++ dasarWeb/Pertemuan7/html_aman.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Injection</title>
+    </head>
+    <body>
+        <form method="post" action="<?PHP echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+            <label for="nama">Nama:</label>
+            <input type="text" name="input" id="nama" required><br><br>
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" required><br><br>
+            <input type="submit" name="submit" id="submit">
+        </form>
+    </body>
+</html>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $input = $_POST['input'];
+    $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+    echo $input;
+}
+?>
