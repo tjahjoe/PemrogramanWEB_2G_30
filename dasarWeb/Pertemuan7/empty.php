3bd01321324781f2d5b063ab54c0ddd401f8f1b5
tjahjoe <ntell634@gmail.com>
2024-10-10 10:09:36 +0700

    P7_Praktikum2_Soal2.2_30

--- dasarWeb/Pertemuan7/empty.php
+++ dasarWeb/Pertemuan7/empty.php
@@ -6,3 +6,11 @@ if (empty($muArray)) {
     echo "Array terdefinisi dan tidak kosong.";
 }
 ?>
+<br>
+<?php
+if (empty($nonExistenVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.";
+}
+?>
