a7a47e6fdf873b0e47811b26c97c15fde70e1a98
tjahjoe <ntell634@gmail.com>
2024-10-10 09:44:04 +0700

    P7_Praktikum1_Soal1.2_30

--- dasarWeb/Pertemuan7/isset.php
+++ dasarWeb/Pertemuan7/isset.php
@@ -1,8 +1,17 @@
 <?php
-$umur;
-if (isset($umur) && $umur >= 10) {
-    echo "Anda sudah dewasa.";
+// $umur;
+// if (isset($umur) && $umur >= 10) {
+//     echo "Anda sudah dewasa.";
+// } else {
+//     echo "Anda belum dewasa atau variabel 'umur' tidak ditentukan.";
+// }
+?>
+
+<?php
+$data= array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
 } else {
-    echo "Anda belum dewasa atau variabel 'umur' tidak ditentukan.";
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
 }
 ?>
