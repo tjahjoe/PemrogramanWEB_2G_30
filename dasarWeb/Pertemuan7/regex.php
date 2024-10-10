d0f77f0eeed316a1f095f20cbc7e52402ca4d176
tjahjoe <ntell634@gmail.com>
2024-10-10 18:00:46 +0700

    P7_Praktikum5_Soal5.5_30

--- dasarWeb/Pertemuan7/regex.php
+++ dasarWeb/Pertemuan7/regex.php
@@ -19,15 +19,25 @@
 ?>
 
 <?php
-$pattern = '/apple/';
-$replacement = 'banana';
-$text = 'I like apple pie.';
-$new_text = preg_replace($pattern, $replacement, $text);
-echo $new_text;
+// $pattern = '/apple/';
+// $replacement = 'banana';
+// $text = 'I like apple pie.';
+// $new_text = preg_replace($pattern, $replacement, $text);
+// echo $new_text;
 ?>
 
 <?php
-$pattern ='/go*d/';
+// $pattern ='/go*d/';
+// $text = 'god is good';
+// if (preg_match($pattern, $text, $matches)) {
+//     echo 'Cocokkan: ' .$matches[0];
+// } else {
+//     echo 'Tidak ada yang cocok!';
+// }
+?>
+
+<?php
+$pattern ='/g?/';
 $text = 'god is good';
 if (preg_match($pattern, $text, $matches)) {
     echo 'Cocokkan: ' .$matches[0];
