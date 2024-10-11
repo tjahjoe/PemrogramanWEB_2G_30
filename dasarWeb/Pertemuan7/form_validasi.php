397a4efbffd52a81e1f814154ff6fea63635be3e
tjahjoe <ntell634@gmail.com>
2024-10-11 07:07:00 +0700

    P7_Praktikum7_Soal7.1_30

---
+++ dasarWeb/Pertemuan7/form_validasi.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form Input dengan Validasi</title>
+    </head>
+    <body>
+        <h1>Form Input dengan Validasi</h1>
+        <form method="post" action="proses_validasi.php">
+        <label for="nama">Nama:</label>
+        <input type="text" name="nama" id="nama">
+
+        <br>
+
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email">
+
+        <br>
+
+        <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
---
+++ dasarWeb/Pertemuan7/proses_validasi.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
+    $errors = array();
+    if (empty($nama)) {
+        $errors[] = 'Nama harus diisi.'; 
+    }
+
+    if (empty($email)) {
+        $errors[] = 'Email harus diisi.';
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Format email tidak valid.';
+    }
+
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo $error . '<br>';
+        }
+    } else {
+        echo "Data berhasil dikirim: Nama = $nama, Email = $email";
+    }
+}
+?>
