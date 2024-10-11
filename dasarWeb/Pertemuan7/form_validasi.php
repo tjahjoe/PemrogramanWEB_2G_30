aebe71d1d5e8ee9ba0b0df73a7f8433e7b4d06f1
tjahjoe <ntell634@gmail.com>
2024-10-11 08:25:27 +0700

    P7_Praktikum7_Soal7.3_30

--- dasarWeb/Pertemuan7/form_validasi.php
+++ dasarWeb/Pertemuan7/form_validasi.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <h1>Form Input dengan Validasi</h1>
-        <form id="myForm" method="post" action="proses_validasi.php">
+        <form id="myForm">
         <label for="nama">Nama:</label>
         <input type="text" name="nama" id="nama">
         <span id="nama-error" style="color : red;"></span>
@@ -22,32 +22,44 @@
         <input type="submit" value="Submit">
         </form>
 
+        <div id="hasil" >
+
+        </div>
+
         <script>
             $(document).ready(function(){
                 $("#myForm").submit(function(event){
-                    var nama = $("#nama").val()
-                    var email = $("#email").val()
-                    var valid = true
+                    // var nama = $("#nama").val()
+                    // var email = $("#email").val()
+                    // var valid = true
 
-                    if (nama == "") {
-                        $("#nama-error").text("Nama harus diisi.")
-                        valid = false
-                    } else {
-                        $("#nama-error").text("")
-                    }
+                    // if (nama == "") {
+                    //     $("#nama-error").text("Nama harus diisi.")
+                    //     valid = false
+                    // } else {
+                    //     $("#nama-error").text("")
+                    // }
                     
-                    if (email == "") {
-                        $("#email-error").text("Email harus diisi.")
-                        valid = false
-                    } else {
-                        $("#email-error").text("")
-                    }
+                    // if (email == "") {
+                    //     $("#email-error").text("Email harus diisi.")
+                    //     valid = false
+                    // } else {
+                    //     $("#email-error").text("")
+                    // }
 
-                    if (!valid) {
-                        event.preventDefault()
-                    }
-
-                    
+                    // if (!valid) {
+                    //     event.preventDefault()
+                    // }
+                    event.preventDefault()
+                    var formData = $("#myForm").serialize()
+                    $.ajax({
+                        url: "proses_validasi.php",
+                        type: "POST",
+                        data:formData,
+                        success:function(response){
+                            $("#hasil").html(response)
+                        }
+                    })        
                 })
             })
         </script>
