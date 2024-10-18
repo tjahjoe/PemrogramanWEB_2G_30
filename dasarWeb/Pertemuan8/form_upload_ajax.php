24da2891d546967a1256b883f1af9264208de446
tjahjoe <ntell634@gmail.com>
2024-10-18 10:50:08 +0700

    P8_Praktikum3_Soal11_30

--- dasarWeb/Pertemuan8/form_upload_ajax.php
+++ dasarWeb/Pertemuan8/form_upload_ajax.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data" >
-            <input type="file" name="file" id="file" >
+            <input type="file" name="file[]" multiple="multiple" id="file" >
             <input type="submit" name="submit" value="Unggah" >
         </form>
         <div id="status" ></div>
--- dasarWeb/Pertemuan8/upload_ajax.php
+++ dasarWeb/Pertemuan8/upload_ajax.php
@@ -1,26 +1,30 @@
 <?php
-if (isset(($_FILES['file']))) {
+if (isset(($_FILES['file']['name']))) {
+    $totalFiles = count($_FILES['file']['name']);
     $errors = array();
-    $file_name = $_FILES['file']['name'];
-    $file_size = $_FILES['file']['size'];
-    $file_tmp = $_FILES['file']['tmp_name'];
-    $file_type = $_FILES['file']['type'];
-    @$file_ext = strtolower("" . end(explode('.', $_FILES['file']['name'])) . "");
     $extensions = array("pdf", "doc", "docx", "txt");
+    for ($i = 0; $i < $totalFiles; $i++) {
+        $file_name = $_FILES['file']['name'][$i];
+        $file_size = $_FILES['file']['size'][$i];
+        $file_tmp = $_FILES['file']['tmp_name'][$i];
+        $file_type = $_FILES['file']['type'][$i];
+        @$file_ext = strtolower("" . end(explode('.', $_FILES['file']['name'][$i])) . "");
+        
 
-    if(in_array($file_ext, $extensions) === false) {
-        $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, atau TXT.";
-    }
+        if (in_array($file_ext, $extensions) === false) {
+            $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, atau TXT.";
+        }
 
-    if ($file_size > 2097152) {
-        $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
-    }
+        if ($file_size > 2097152) {
+            $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
+        }
 
-    if (empty($errors) == true) {
-        move_uploaded_file($file_tmp, "documents/" . $file_name);
-        echo "File berhasil diunggah.";
-    } else {
-        echo implode(" ", $errors);
+        if (empty($errors) == true) {
+            move_uploaded_file($file_tmp, "documents/" . $file_name);
+            echo "File berhasil diunggah.";
+        } else {
+            echo implode(" ", $errors);
+        }
     }
 }
 ?>
