e877475ba6551f67ed4ab314d4585034b1abb41c
tjahjoe <ntell634@gmail.com>
2024-10-18 12:10:45 +0700

    P8_Praktikum3_Soal12_30_Revisi

--- dasarWeb/Pertemuan8/form_upload_ajax.php
+++ dasarWeb/Pertemuan8/form_upload_ajax.php
@@ -1,15 +1,25 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Unggah File Dokumen</title>
-    </head>
-    <body>
-        <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data" >
-            <input type="file" name="file[]" multiple="multiple" id="file" >
-            <input type="submit" name="submit" value="Unggah" >
+
+<head>
+    <link rel="stylesheet" type="text/css" href="upload.css">
+    <title>Unggah File Dokumen</title>
+</head>
+
+<body>
+    <div class="upload-form-container">
+        <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
+            <div class="file-input-container">
+                <input type="file" name="file" id="file" class="file-input">
+                <label for="file" class="file-label">Pilih File</label>
+            </div>
+            <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
         </form>
-        <div id="status" ></div>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script>
-        <script src="upload.js" ></script>
-    </body>
+        <div id="status"></div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="upload.js"></script>
+</body>
+
 </html>
--- dasarWeb/Pertemuan8/upload.js
+++ dasarWeb/Pertemuan8/upload.js
@@ -1,22 +1,28 @@
 $(document).ready(function () {
-    $('#upload-form').submit(function(e){
-        e.perventDefault()
-        var formData = new FormData(this)
+    $('#file').change(function() {
+        if (this.files.length > 0) {
+            $('#upload-button').prop('disabled', false).css('opacity', 1);
+        } else {
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+        }
+    });
 
+    $('#upload-form').submit(function(e) {
+        e.preventDefault()
+        var formData = new FormData(this);
         $.ajax({
             type: 'POST',
-            url : 'upload_ajax.php',
-            data : formData,
-            chace : false,
-            contentType : false,
-            processData : false,
-            success : function(response){
-                $('#status').html(response)
+            url: 'upload_ajax.php',
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                $('#status').html(response);
             },
-            error : function () {
-                $('#status').html('Terjadi kesalahan saat mengunggah file.')
+            error: function() {
+                $('#status').html('Terjadi kesalahan saat mengunggah file.');
             }
-            
-        })
-    })
-})
+        });
+    });
+});
