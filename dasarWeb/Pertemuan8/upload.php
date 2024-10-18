7d1293d5d525c9e83accb1059169f85a1ff83506
tjahjoe <ntell634@gmail.com>
2024-10-18 07:56:02 +0700

    P8_Praktikum1_Soal6_30

--- dasarWeb/Pertemuan8/upload.php
+++ dasarWeb/Pertemuan8/upload.php
@@ -4,8 +4,8 @@ if (isset($_POST['submit'])) {
     $targetfile = $targetdir . basename($_FILES['myfile']['name']);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    $allowedExtensions = array('jpg', 'jpeg', 'png', 'gif');
-    $maxsize = 5 * 1024 * 1024;
+    $allowedExtensions = array('txt', 'pdf', 'doc', 'docx');
+    $maxsize = 3 * 1024 * 1024;
     if (in_array($fileType, $allowedExtensions) && $_FILES['myfile']['size'] <= $maxsize) {
         if (move_uploaded_file($_FILES['myfile']['tmp_name'], $targetfile)) {
             echo 'File berhasil diunggah <br>';
